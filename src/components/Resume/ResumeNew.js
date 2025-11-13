883b17ed920023aec1a9415ca9b0a61756a4539d
divyesh0406 <divyeshrmistry@gmail.com>
2025-11-12 16:56:29 -0800

    Update Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,7 +4,8 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 // import pdf from "../../Assets/../Assets/DivyeshResume.pdf";
 // import pdf from "../../Assets/../Assets/DivyeshMistry-Resume.pdf";
-import pdf from "../../Assets/../Assets/Divyesh_Mistry_Resume.pdf";
+import pdf from "../../Assets/../Assets/Divyesh-Mistry-Resume.pdf";
+
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
